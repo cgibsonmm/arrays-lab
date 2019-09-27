544bd5a5b84f042fea2a127fbbfda8dd4852f6b8
Bruno Galvao <brunopgalvao@gmail.com>
2019-09-27 14:55:01 -0400

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,52 @@
+/**
+ * Arrays
+ * All of your answers should be stored in
+ * variables matching the question numbers (q1, q2, etc.)
+ *
+ * (e.g.) const q1 = 'my answer here';
+ */
+
+/**
+ * QUESTION 1
+ * Create an array of image sources.
+ * Use image1.png, image2.png, and image3.png as the array values.
+ */
+
+
+/**
+ * QUESTION 2
+ * Using the array from Question 1, store the first element of the array in variable q2.
+ */
+
+
+/**
+ * QUESTION 3
+ * Get the length of the first array (number of elements in the array) and store it in variable q3
+ */
+
+
+/**
+ * QUESTION 4
+ * Using the array from Question 1,
+ * store the last element of the array
+ * in variable q4.
+ * Hint: How can we get the number of elements in the array?
+ */
+
+
+/**
+ * QUESTION 5
+ * Using the array from Question 1,
+ * add the item, image4.png to the array.
+ * Store the result in variable q5.
+ * Hint: Refer to the array methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+ 
+ 
+/**
+ * QUESTION 6
+ * Using the array from Question 1,
+ * remove the image2.png and image3.png from the array.
+ * Store the result in variable q6.
+ * Hint: Refer to the array methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
