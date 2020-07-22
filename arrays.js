b394ce84980c1636d6974c2337145acf7c841446
Steve VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-07-22 11:57:19 -0600

    Added question and clarity.

--- arrays.js
+++ arrays.js
@@ -15,38 +15,41 @@
 
 /**
  * QUESTION 2
- * Using the array from Question 1, store the first element of the array in variable q2.
+ * Using the array from Question 1, store the first element of the array in variable q2. Do not modify the array.
  */
 
 
 /**
  * QUESTION 3
- * Get the length of the first array (number of elements in the array) and store it in variable q3
+ * Get the length (number of elements in the array) of the first array q1, and store it in variable q3
  */
 
 
 /**
  * QUESTION 4
- * Using the array from Question 1,
- * store the last element of the array
- * in variable q4.
+ * Using the array from Question 1, store the last element of the array in variable q4. Do not modify the original array.
  * Hint: How can we get the number of elements in the array?
  */
 
 
 /**
  * QUESTION 5
- * Using the array from Question 1,
- * add the item, image4.png to the array.
- * Store the result in variable q5.
+ * Using the array from Question 1, add the item, image4.png to the original array.
+ * Store the new array in variable q5.
  * Hint: Refer to the array methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
  */
  
  
 /**
  * QUESTION 6
- * Using the array from Question 1,
- * remove the image2.png and image3.png from the array.
+ * Using the array from Question 1, remove image2.png and image3.png from the original array without modifying the original array.
  * Store the result in variable q6.
+ * Hint: Refer to the array methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+
+/**
+ * QUESTION 7
+ * Using the array from Question 1, remove image2.png and image3.png from the original array, modifying the original array.
+ * Store the result in variable q7.
  * Hint: Refer to the array methods: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
  */
